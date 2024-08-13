daf2ace13748be67f8eefae83420ad51a286a525
Bruno Oliveira <164297113+DevBuu@users.noreply.github.com>
2024-08-13 12:05:11 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+
+package primeiroprograma;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo");
+        System.out.println("Meu inicio em Javaaaaa!!");
+        System.out.println("Hello Github!!!");
+    } 
+}
